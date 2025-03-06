0d13589ece465e8ef6f8ac6bad54c23ab88851f8
taira-no-atsumori <syun.freedom@gmail.com>
2025-03-06 17:39:20 +0900

    pwaTest

---
+++ pwa-assets.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@vite-pwa/assets-generator/config'
+
+export default defineConfig({
+  images: [
+    'public/vite.svg',
+  ]
+})
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'PWAテストアプリ',
+        short_name: 'PWAテスト',
+        description: 'PWA test',
+        theme_color: '#e6eeff',
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
+})
